456416627555855486d033aa5c6f941d17737e3d
Priyanshu <ribupkashyap@gmail.com>
2025-12-21 19:28:08 +0530

    Added authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import LogOut from "@/module/auth/components/logout";
+import { requireAuth } from "@/module/auth/utils/auth-utils";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  await requireAuth();
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold mb-8">Welcome to My Next.js App!</h1>
-      <Button>Click me</Button>
+      <LogOut><Button className="cursor-pointer">Log Out</Button></LogOut>
     </div>
   );
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,19 +1,16 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Return } from "@prisma/client/runtime/client";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL || "",
+  connectionString: process.env.DATABASE_URL,
 });
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    adapter,
-  });
+  return new PrismaClient({adapter});
 };
 
 declare const globalThis: {
-    prismaGlobal: Return<typeof prismaClientSingleton>;
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
 const prisma = globalThis.prismaGlobal || prismaClientSingleton();
